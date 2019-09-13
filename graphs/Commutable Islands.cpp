0e6d583a75b1027d32fa687154d99ede46e101b4
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-09-14 02:50:16 +0530

    Update Commutable Islands.cpp

--- graphs/Commutable Islands.cpp
+++ graphs/Commutable Islands.cpp
@@ -1,69 +1,64 @@
-vector<pair<pair<int, int>, int>> cost;
-vector<int> parent, r;
-int vertex, edge;
-bool sortCol(const pair<pair<int, int>, int>& x, const pair<pair<int, int>, int>& y)
+/*
+I am doing it using kruskal algo
+I am takink rank is how many children, the parent has.
+I m implementing path compression, otherwise you will get TLE.
+*/
+vector<int> parent, rank1;
+int ans;
+bool compare(vector<int> a, vector<int> b)
 {
-    return (x.second<y.second);
+    int x=a[2];
+    int y=b[2];
+    return (x<y);
 }
-
-
-
 int find_set(int u)
 {
     int x=parent[u];
     if(x!=u)
-        x=find_set(x);
-    return x;
+    {
+        parent[u]=find_set(x);
+    }
+    return parent[u];
 }
 
 void merge(int u, int v)
 {
-    int x=find_set(u);
-    int y=find_set(v);
-    if(r[x]<r[y])
-    parent[x]=y;
+    int parent_u=find_set(u);
+    int parent_v=find_set(v);
+    int rank_u=rank1[parent_u];
+    int rank_v=rank1[parent_v];
+    if(rank_u<rank_v)
+    {
+        parent[parent_u]=parent_v;
+        rank1[parent_v]+=rank_u+1;
+    }
     else
-    parent[y]=x;
-    if(r[y]==r[x])
-    r[x]++;
+    {
+        parent[parent_v]=parent_u;
+        rank1[parent_u]+=rank_v+1;
+    }
     
 }
-void createSet()
-{
-    for(int i=1;i<=vertex;i++)
+int Solution::solve(int A, vector<vector<int> > &B) {
+    sort(B.begin(), B.end(),compare);
+    parent.resize(A+1);
+    rank1.resize(A+1, -1);
+    int n=B.size();
+    ans=0;
+    for(int i=1;i<=A;i++)
     {
         parent[i]=i;
-        r[i]=0;
+        rank1[i]=1;
     }
-}
-
-int kruskal()
-{
-    createSet();
-    int minCost=0;
-    for(int i=0;i<edge;i++)
+    for(int i=0;i<n;i++)
     {
-        int u=cost[i].first.first;
-        int v=cost[i].first.second;
+        int u=B[i][0];
+        int v=B[i][1];
         if(find_set(u)!=find_set(v))
         {
-            minCost+=cost[i].second;
+            ans+=B[i][2];
             merge(u, v);
         }
     }
-    return minCost;
-}
-int Solution::solve(int A, vector<vector<int> > &B) {
-    cost.clear();
-    edge=B.size();
-    vertex=A;
-    r.resize(A+1);
-    parent.resize(A+1);
-    for(int i=0;i<edge;i++)
-    {
-       cost.push_back({{B[i][0], B[i][1]}, B[i][2]});
-    }
-    sort(cost.begin(), cost.end(), sortCol);
-    return kruskal();
-    
+    return ans;
 }
