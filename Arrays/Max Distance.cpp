39249609727512732d3a3689270a6c716fce9421
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-09-14 06:08:57 +0530

    Google, Amazon, Microsoft

--- Arrays/Max Distance.cpp
+++ Arrays/Max Distance.cpp
@@ -1,18 +1,23 @@
 int Solution::maximumGap(const vector<int> &A) {
-    if(A.size()==1)return 0;
-        if(A.size()==0)return -1;
-    vector<pair<int, int>> index;
+    vector<pair<int,int>> num(A.size());
     for(int i=0;i<A.size();i++)
     {
-        index.push_back(make_pair(A[i],i));
+        num[i].first=A[i];
+        num[i].second=i;
     }
-    sort(index.begin(),index.end());
-    int max_distance=INT_MIN,max_index=index[A.size()-1].second;
-    for(int i=A.size()-2;i>=0;i--)
+    sort(num.begin(), num.end());
+    int ans=0;
+    int min=num[0].second;
+    for(int i=0;i<A.size()-1;i++)
     {
-        max_distance=max(max_distance, max_index-index[i].second);
-        max_index=max(max_index,index[i].second);
+        if(num[i+1].second>min)
+        {
+            if(num[i+1].second-min>ans)
+            ans=num[i+1].second-min;
+        }
+        else
+        min=num[i+1].second;
+        
     }
-    if(max_distance<0){return 0;}
-    else return max_distance;
+    return ans;
 }
