c32450ca12b9200392d40559c3be7b97ed86f59e
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-09-15 00:01:29 +0530

    Update Partition List.cpp

--- Linked Lists/Partition List.cpp
+++ Linked Lists/Partition List.cpp
@@ -53,3 +53,61 @@ ListNode* Solution::partition(ListNode* A, int B) {
     
     
 }
+#########################  Solution 2 #####################################
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::partition(ListNode* A, int B) {
+    ListNode *less=NULL;
+    ListNode* greater=NULL;
+    ListNode* head, *head1;
+    ListNode *temp=A;
+    while(temp)
+    {
+        ListNode* temp1=temp->next;
+        if(temp->val<B)
+        {
+            if(!less)
+            {
+                less=temp;
+                head=temp;
+                less->next=NULL;
+            }
+            else
+            {
+                less->next=temp;
+                less=less->next;
+                less->next=NULL;
+            }
+        }
+        else
+        {
+            if(!greater)
+            {
+                greater=temp;
+                head1=temp;
+                greater->next=NULL;
+            }
+            else
+            {
+                greater->next=temp;
+                greater=greater->next;
+                greater->next=NULL;
+            }
+        }
+        temp=temp1;
+    }
+    if(less)
+    {
+        less->next=head1;
+        return head;
+    }
+    return head1;
+    
+}
+
