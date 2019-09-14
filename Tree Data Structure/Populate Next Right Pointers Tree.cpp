ed206ec2e98edcc1ee83195ce565ad16ff3b435f
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-09-14 21:51:11 +0530

    Update Populate Next Right Pointers Tree.cpp

--- Tree Data Structure/Populate Next Right Pointers Tree.cpp
+++ Tree Data Structure/Populate Next Right Pointers Tree.cpp
@@ -54,3 +54,53 @@ void Solution::connect(TreeLinkNode* A) {
         }
     }
 }
+
+###########################             Solution 3 (constant space)       ###################################
+/**
+ * Definition for binary tree with next pointer.
+ * struct TreeLinkNode {
+ *  int val;
+ *  TreeLinkNode *left, *right, *next;
+ *  TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+ * };
+ */
+void Solution::connect(TreeLinkNode* A) {
+    TreeLinkNode *leftWall=NULL;
+   TreeLinkNode *prev=NULL;
+   TreeLinkNode *cur=A;
+   while(cur!=NULL)
+   {
+       while(cur!=NULL)
+       {
+           if(cur->left)
+           {
+               if(prev)
+               {
+                   prev->next=cur->left;
+               }
+               else
+               {
+                   leftWall=cur->left;
+               }
+               prev=cur->left;
+           }
+           
+           if(cur->right)
+           {
+               if(prev)
+               {
+                   prev->next=cur->right;
+               }
+               else
+               {
+                   leftWall=cur->right;
+               }
+               prev=cur->right;
+           }
+           cur=cur->next;
+       }
+       cur=leftWall;
+       leftWall=NULL;
+       prev=NULL;
+   }
+}
