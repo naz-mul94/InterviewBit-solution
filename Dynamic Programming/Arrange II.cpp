e77c2c8064aef24b22a8ed6ff01845a69d0d6ca0
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-09-17 06:34:09 +0530

    Create Arrange II.cpp

---
+++ Dynamic Programming/Arrange II.cpp
@@ -0,0 +1,4 @@
+/*
+Let's assume there are six horses BBWBWB where B represents black horses and W represents white horses. And there are three stables.
+This problem is alike to matrix multication problem. We have to decide where should we put parenthesis
+*/
