1a153c500b4a55581780758d7bc091a5a082de5a
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-11-15 00:52:46 +0530

    Update Longest Increasing Subsequence.cpp

--- Dynamic Programming/Longest Increasing Subsequence.cpp
+++ Dynamic Programming/Longest Increasing Subsequence.cpp
@@ -18,3 +18,31 @@ int Solution::lis(const vector<int> &A) {
     }
     return ans;
 }
+
+
+###################################  O(nlong) solution ##########################################
+
+int Solution::lis(const vector<int> &A) 
+{
+   vector<int> ans;
+   int len=-1;
+   if(A.size()==0)return 0;
+   if(A.size()==1)return 1;
+   ans.push_back(A[0]);
+   len++;
+   for(int i=1;i<A.size();i++)
+   {
+       if(A[i]<ans[0])ans[0]=A[i];
+       else if(ans[len]<A[i])
+       {
+           ans.push_back(A[i]);
+           len++;
+       }
+       else
+       {
+           auto x=lower_bound(ans.begin(), ans.end(), A[i]);
+           ans[x-ans.begin()]=A[i];
+       }
+   }
+   return ++len;
+}
