0d97b421c8ab7f437ae528b06232fc3065ab919d
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2019-12-20 20:17:03 +0530

    Create Inorder Traversal.cpp

---
+++ Tree Data Structure/Inorder Traversal.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+    vector<int> ans;
+    stack<TreeNode *> bucket;
+    if(A==NULL)return ans;
+    bucket.push(A);
+    while(!bucket.empty())
+    {
+        TreeNode * temp=bucket.top();
+        
+        if(temp)
+        {
+            
+            
+            
+            if(temp->left)
+            {
+                bucket.push(temp->left);
+                temp->left=NULL;
+            }
+            else 
+            {
+                
+                ans.push_back(temp->val);
+                bucket.pop();
+                if(temp->right)
+                bucket.push(temp->right);
+                
+            }
+            
+        }
+    }
+    return ans;
+}
