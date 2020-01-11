dbd25b9e0c7262c0208302ac897cbdfb9e15f033
NAZMUL HUSSAIN <nazmulmpdm94@gmail.com>
2020-01-11 23:26:52 +0530

    Update Postorder Traversal.cpp

--- Tree Data Structure/Postorder Traversal.cpp
+++ Tree Data Structure/Postorder Traversal.cpp
@@ -7,6 +7,8 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+//Note: Follow 1st method always.
 vector<int> Solution::postorderTraversal(TreeNode* A) {
     vector<int> ans;
     stack<TreeNode *> bucket;
@@ -39,3 +41,60 @@ vector<int> Solution::postorderTraversal(TreeNode* A) {
     }
     return ans;
 }
+
+################################## different method ###################################################
+vector<int> Solution::postorderTraversal(TreeNode* root) 
+{
+    vector<int> ans;// for storing the result
+    stack<TreeNode*> st;
+    TreeNode* temp, *current;// temp for storing intermediate node and current for traversing the tree
+    if(!root)return ans;// checking root null or not
+    if(root->left)// if left Node present, go to left side
+    {
+        current=root->left;
+        root->left=NULL;
+        st.push(root);
+    }
+    else if(root->right)// if right node present, traverse towards right side
+    {
+        current=root->right;
+        root->right=NULL;
+        st.push(root->right);
+    }
+    else//if there is no child
+    {
+        ans.push_back(root->val);
+        return ans;
+    }
+    
+    while(!st.empty() || current)
+    {
+        if(current->left)
+        {
+            temp=current->left;
+            current->left=NULL;
+            st.push(current);
+            current=temp;
+        }
+        else if(current->right)
+        {
+            temp=current->right;
+            current->right=NULL;
+            st.push(current);
+            current=temp;
+        }
+        else
+        {
+            ans.push_back(current->val);
+            if(!st.empty())
+            {
+                current=st.top();
+                st.pop();
+            }
+            else
+            current=NULL;
+        }
+    }
+    return ans;
+}
+
